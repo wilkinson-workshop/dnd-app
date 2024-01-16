d00e5b4776caf35c75ae93caf367ba3d8f472119
WilkinsonK <keenanwilkinson@outlook.com>
2024-01-16 11:14:57 -0600

    reorganize project - moved CLI components to separate module - moved app comonents to `app.py` module - defined route declaration pattern

---
+++ src/scryer/app.py
@@ -0,0 +1,115 @@
+"""
+The HTTP server core implementation.
+"""
+
+import pathlib
+
+# Third-party dependencies.
+import click
+import uvicorn
+from fastapi import APIRouter, FastAPI, WebSocket
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+# Root directory appliction is being executed
+# from. Will be used for creating and
+# fetching assets related to the application.
+EXECUTION_ROOT = pathlib.Path.cwd()
+
+# -----------------------------------------------
+# Web/HTTP Application Defintion.
+# -----------------------------------------------
+# Initialize the application config in static
+# space.
+app = FastAPI()
+
+
+def setup_application():
+    """
+    A factory function responsible for finalizing
+    the appliction on each reload.
+    """
+
+    for mount in ASGI_APP_MOUNTS:
+        app.mount(*mount)
+
+    for _, router in APP_ROUTERS.items():
+        app.include_router(router)
+
+    return app
+
+
+# -----------------------------------------------
+# External ASGI applications.
+# -----------------------------------------------
+# The defined applications are to be mounted by
+# the root application later. Will be used for
+# serving additional materials to the front-end.
+ASGI_APP_MOUNTS = (
+    ("/static", StaticFiles(directory=EXECUTION_ROOT / "static", html=True)),
+)
+
+# -----------------------------------------------
+# API Routes.
+# -----------------------------------------------
+APP_ROUTERS = {
+    # Defines the routes available at the root of our
+    # HTTP server.
+    "root": APIRouter(),
+    # Defines the routes available to managing
+    # game sessions.
+    "session": APIRouter()
+}
+
+@APP_ROUTERS["root"].get("/healthcheck")
+@APP_ROUTERS["root"].head("/healthcheck")
+async def healthcheck():
+    """
+    Ping the server, and its services, to see if
+    it is online and available.
+    """
+    # TODO: implement healthcheck to ensure all
+    # services are running.
+    return "online"
+
+
+@APP_ROUTERS["root"].get("/healthcheck/{service}")
+async def healthcheck_service(service: str):
+    """
+    Ping the server, and its services, to see if
+    it is online and available.
+    """
+    # TODO: implement healtcheck handling for
+    # internal services.
+    return "online"
+
+
+@APP_ROUTERS["session"].patch("/sessions/{idn}")
+async def sessions_join(idn: str):
+    """Join an active session."""
+
+    return NotImplemented
+
+
+@APP_ROUTERS["session"].get("/sessions")
+async def sessions_list():
+    """List active sessions."""
+
+    return NotImplemented
+
+
+@APP_ROUTERS["session"].post("/sessions")
+async def sessions_make():
+    """Create a new joinable session."""
+
+    return NotImplemented
+
+
+@APP_ROUTERS["session"].websocket("socket")
+async def session_sock(sock: WebSocket):
+    """Initiate a session `WebSocket`"""
+
+    await sock.accept()
+    while True:
+        data = await sock.receive_text()
+        await sock.send_text(f"Got {data!s} from connection.")
---
+++ src/scryer/cli.py
@@ -0,0 +1,53 @@
+"""
+The CLI application. A shortcut script for loading
+our application as configured per our
+preferrences.
+"""
+import subprocess
+
+import click
+import uvicorn
+
+
+# -----------------------------------------------
+# Command Line Interface (CLI).
+# -----------------------------------------------
+# Defines `scryer` as the entry point for managing
+# our application.
+@click.group(name="scryer")
+def main():
+    """Manage this application."""
+
+
+@main.command()
+def make():
+    """
+    Build this project and install into the
+    environment.
+    """
+
+    # Will install this project into the active
+    # environment.
+    subprocess.call(["pip3", "install", "-U", "."])
+
+
+@main.command()
+@click.option("-H", "--hostname", default="localhost")
+@click.option("-p", "--port", default=8000)
+@click.option("-W", "--workers", type=int, default=None)
+def start(hostname: str, port: int, *, workers: int | None):
+    """Starts the web server."""
+
+    kwds = dict(factory=True, host=hostname, port=port, reload=True)
+    if workers:
+        # Workers are ignored when 'reload' is
+        # enabled. Might not want to implicitly
+        # disable reload in this context.
+        kwds["workers"] = workers
+        kwds["reload"]  = False
+
+    uvicorn.run("scryer.app:setup_application", **kwds) #type: ignore[arg-type]
+
+
+if __name__ == "__main__":
+    exit(main())
