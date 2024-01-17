7c347ac9dbe1b5935f0921c9be54444066d29fb9
WilkinsonK <keenanwilkinson@outlook.com>
2024-01-16 21:55:18 -0600

    add local persistent storage session broker

--- src/scryer/sessions.py
+++ src/scryer/sessions.py
@@ -1,6 +1,15 @@
-import asyncio
 import enum
 import typing
+import uuid
+
+try:
+    # This module is potentially not implemented
+    # depending on the platform and the packages
+    # available to said system.
+    import shelve
+    SHELVE_IMPLEMENTED = True
+except ImportError:
+    SHELVE_IMPLEMENTED = False
 
 from fastapi import WebSocket
 
@@ -52,7 +61,7 @@ class HitPoints(int):
         return max(self - other, self.min_hitpoints)
 
 
-class PlayerRole(enum.StrEnum):
+class Role(enum.StrEnum):
     """
     The role assigned to a player per session.
     """
@@ -63,13 +72,13 @@ class PlayerRole(enum.StrEnum):
 
 
 class User(typing.Protocol):
+    @property
     def is_active(self) -> bool:
         """
         User is currently online and in a
         session.
         """
-    def role(self, session: "Session") -> PlayerRole:
-        """The role assigned to this player."""
+    @property
     def session(self) -> "Session":
         """`Session` user is connected to."""
 
@@ -80,43 +89,54 @@ class Creature(typing.Protocol):
     active in a game session.
     """
 
+    @property
     def conditions(self) -> list[Condition]:
         """Active conditions on this creature."""
+    @property
     def hit_points(self) -> HitPoints:
         """
         Returns the hit-points (HP) of this
         creature.
         """
+    @property
     def is_player(self) -> bool:
         """
         Whether creater is a Player Character or a
         Non-Player Character.
         """
+    @property
     def owner(self) -> User:
         """The user who 'owns' this creature."""
 
+    def role(self, session: "Session") -> Role:
+        """The role assigned to this creature."""
 
-class Session[C: WebSocket, **P](typing.Protocol):
+
+class Session[C: WebSocket](typing.Protocol):
     """
     Active session that manages connections and
     action requests available to users.
     """
 
+    @classmethod
+    def new_instance(cls) -> typing.Self:
+        """Creates a new session instance."""
+
+    @property
+    def label(self) -> str:
+        """Identity of this session."""
+    @property
+    def clients(self) -> typing.Sequence[C]:
+        """Active client connections."""
+
     async def attach_client(self, client: C):
         """Process a `connect` request."""
-
-    # [inline]
-    async def broadcast_action(self, action: Action[C, P]) -> typing.Sequence[tuple[C, int]]:
+    async def broadcast_action[**P](self, action: Action[C, P]) -> typing.Sequence[tuple[C, int]]:
         """
         Do an action against all connections.
         Returns the number of bytes sent to each
         client.
         """
-
-        return await asyncio.gather(*[action(c) for c in self.clients()])
-
-    def clients(self) -> typing.Sequence[C]:
-        """Active client connections."""
     async def delete(self):
         """
         Stop all connections and perform any
@@ -124,6 +144,11 @@ class Session[C: WebSocket, **P](typing.Protocol):
         """
     async def detach_client(self, client: C):
         """Disconnect a client."""
+    async def do_action[**P](self, client: C, action: Action[C, P]) -> tuple[C, int]:
+        """
+        Serve an action to the target client
+        connection.
+        """
 
 
 class SessionBroker(typing.Protocol):
@@ -132,7 +157,7 @@ class SessionBroker(typing.Protocol):
     appliction and the data layer.
     """
 
-    async def create(self) -> Session:
+    async def create(self) -> (str, Session):
         """
         Create a new `Session` instance, record
         meta to the data layer, then return it.
@@ -148,3 +173,61 @@ class SessionBroker(typing.Protocol):
         Attempt to locate a `Session` from the
         label.
         """
+    async def status(self) -> str:
+        """Connection status of this broker."""
+
+
+class SessionShelver(SessionBroker):
+    """
+    Implementation of `SessionBroker` which stores
+    session data using the `shelve` module.
+
+    This allows data to be stored persistently,
+    but in a way that can be accessed at runtime
+    as if it were in-memory. This would be similar
+    to how we'd want our application to behave in
+    production, but easier to manipulate in
+    development.
+    """
+
+    def __init__(self, shelf_name: str, cls: type[Session]):
+        self.session_cls = cls
+        self.shelf_name  = shelf_name
+        self.shelf       = dict()
+
+    # `__enter__` and `__exit__` are special
+    # methods in Python. They allow us to override
+    # or extend built-in behaviors, and in this
+    # case, allows us to use this class as a
+    # context manager. This is where you'll see
+    # the `with self` statements. Think of
+    # `__enter__` as the setup and `__exit__` as
+    # the teardown.
+    def __enter__(self):
+        if SHELVE_IMPLEMENTED:
+            self.shelf = shelve.open(self.shelf_name)
+        return self
+
+    def __exit__(self, *args):
+        if SHELVE_IMPLEMENTED:
+            self.shelf.close()
+            self.shelf = None
+
+    async def create(self) -> Session:
+        # TODO: move to a separate function.
+        session       = self.session_cls.new_instance()
+        session_label = str(uuid.uuid1())
+        with self:
+            self.shelf[session_label] = session
+        return (session_label, session)
+
+    async def delete(self, label: str):
+        with self:
+            self.shelf.pop(label, None)
+
+    async def locate(self, label: str) -> Session | None:
+        with self:
+            return self.shelf.get(label, None)
+
+    async def status(self) -> str:
+        return "online"
