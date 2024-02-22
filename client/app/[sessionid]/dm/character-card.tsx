00f05c995c8eeca01d6073dee256c91fc9f404fe
joshuahoff1 <joshua.hoff1@gmail.com>
2024-02-22 10:46:05 -0600

    #33 fixed ui when player name has spaces

--- client/app/[sessionid]/dm/character-card.tsx
+++ client/app/[sessionid]/dm/character-card.tsx
@@ -15,167 +15,165 @@ import { Monster } from '@/app/_apis/dnd5eTypings'
 import { MonsterInfoDialog } from './monster-dialog'
 
 const style = {
-  border: '1px solid lightgray',
-  //padding: '0.5rem 1rem',
-  cursor: 'move',
+	border: '1px solid lightgray',
+	cursor: 'move',
 }
 
 export interface CardProps {
-  character: Character,
-  index: number,
-  moveCard: (dragIndex: number, hoverIndex: number) => void,
-  updateCharacter: (character: Character) => void,
-  updateCharacterButton: (character: Character) => void,
-  deleteCharacter: (character: Character) => void
+	character: Character,
+	index: number,
+	moveCard: (dragIndex: number, hoverIndex: number) => void,
+	updateCharacter: (character: Character) => void,
+	updateCharacterButton: (character: Character) => void,
+	deleteCharacter: (character: Character) => void
 }
 
 interface DragItem {
-  index: number
-  id: string
-  type: string
+	index: number
+	id: string
+	type: string
 }
 
 export const Card: FC<CardProps> = ({ character, index, moveCard, updateCharacter, updateCharacterButton, deleteCharacter }) => {
-  const [monsterInfo, setMonsterInfo] = useState<Monster | null>(null);
-  const [open, setOpen] = useState(false);
+	const [monsterInfo, setMonsterInfo] = useState<Monster | null>(null);
+	const [open, setOpen] = useState(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-  
-  const ref = useRef<HTMLDivElement>(null)
-  const [{ handlerId }, drop] = useDrop<
-    DragItem,
-    void,
-    { handlerId: Identifier | null }
-  >({
-    accept: ItemTypes.CARD,
-    collect(monitor) {
-      return {
-        handlerId: monitor.getHandlerId(),
-      }
-    },
-    hover(item: DragItem, monitor) {
-      if (!ref.current) {
-        return
-      }
-      const dragIndex = item.index
-      const hoverIndex = index
+	const handleClose = () => {
+		setOpen(false);
+	};
 
-      // Don't replace items with themselves
-      if (dragIndex === hoverIndex) {
-        return
-      }
+	const ref = useRef<HTMLDivElement>(null)
+	const [{ handlerId }, drop] = useDrop<
+		DragItem,
+		void,
+		{ handlerId: Identifier | null }
+	>({
+		accept: ItemTypes.CARD,
+		collect(monitor) {
+			return {
+				handlerId: monitor.getHandlerId(),
+			}
+		},
+		hover(item: DragItem, monitor) {
+			if (!ref.current) {
+				return
+			}
+			const dragIndex = item.index
+			const hoverIndex = index
 
-      // Determine rectangle on screen
-      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+			// Don't replace items with themselves
+			if (dragIndex === hoverIndex) {
+				return
+			}
 
-      // Get vertical middle
-      const hoverMiddleY =
-        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+			// Determine rectangle on screen
+			const hoverBoundingRect = ref.current?.getBoundingClientRect()
 
-      // Determine mouse position
-      const clientOffset = monitor.getClientOffset()
+			// Get vertical middle
+			const hoverMiddleY =
+				(hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
 
-      // Get pixels to the top
-      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+			// Determine mouse position
+			const clientOffset = monitor.getClientOffset()
 
-      // Only perform the move when the mouse has crossed half of the items height
-      // When dragging downwards, only move when the cursor is below 50%
-      // When dragging upwards, only move when the cursor is above 50%
+			// Get pixels to the top
+			const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
 
-      // Dragging downwards
-      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
-        return
-      }
+			// Only perform the move when the mouse has crossed half of the items height
+			// When dragging downwards, only move when the cursor is below 50%
+			// When dragging upwards, only move when the cursor is above 50%
 
-      // Dragging upwards
-      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
-        return
-      }
+			// Dragging downwards
+			if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+				return
+			}
 
-      // Time to actually perform the action
-      moveCard(dragIndex, hoverIndex)
+			// Dragging upwards
+			if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+				return
+			}
 
-      // Note: we're mutating the monitor item here!
-      // Generally it's better to avoid mutations,
-      // but it's good here for the sake of performance
-      // to avoid expensive index searches.
-      item.index = hoverIndex
-    },
-  })
+			// Time to actually perform the action
+			moveCard(dragIndex, hoverIndex)
 
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemTypes.CARD,
-    item: () => {
-      return { character, index }
-    },
-    collect: (monitor: any) => ({
-      isDragging: monitor.isDragging(),
-    }),
-  });
+			// Note: we're mutating the monitor item here!
+			// Generally it's better to avoid mutations,
+			// but it's good here for the sake of performance
+			// to avoid expensive index searches.
+			item.index = hoverIndex
+		},
+	})
 
-  function getMonsterInfo(monsterId: string){    
-    if(monsterInfo){
-      setOpen(true);
-    } else {    
-      getMonster(monsterId)
-      .then(m => {
-        setMonsterInfo(m);
-        setOpen(true);
-      });
-    }
-  }
+	const [{ isDragging }, drag] = useDrag({
+		type: ItemTypes.CARD,
+		item: () => {
+			return { character, index }
+		},
+		collect: (monitor: any) => ({
+			isDragging: monitor.isDragging(),
+		}),
+	});
 
-  const Item = styled(Box)(({ theme }) => ({
-    padding: theme.spacing(1),  
-  })); 
-  
+	function getMonsterInfo(monsterId: string) {
+		if (monsterInfo) {
+			setOpen(true);
+		} else {
+			getMonster(monsterId)
+				.then(m => {
+					setMonsterInfo(m);
+					setOpen(true);
+				});
+		}
+	}
 
-  const opacity = isDragging ? 0 : 1
-  drag(drop(ref))
-  return (
-    <>
-    <MonsterInfoDialog
-      open={open}
-      monsterInfo={monsterInfo!}
-      onClose={handleClose}
-    /> 
-    <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
-      <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={1} sm={1}>
-            <Item>{character.initiative }</Item>
-          </Grid>
-          <Grid item xs={5} sm={3}>
-            <Item>
-              {character.name}
-              {
-                character.role == CharacterType.NonPlayer ? 
-                (<IconButton aria-label="delete" onClick={() => getMonsterInfo(character.monster!)}>
-                      <InfoIcon />
-                  </IconButton>) : ''
-              }
-            </Item>                        
-          </Grid>
-          <Grid item xs={6} sm={3}>
-            <Item><CharacterHp character={character}/></Item>
-          </Grid>
-          <Grid item xs={8} sm={3}>
-            <Item><CharacterConditions character={character} updateCharacter={updateCharacter}/></Item>
-          </Grid>
-          <Grid item xs={4} sm={2}>
-            <Item style={{"textAlign": "right"}}>
-              <IconButton aria-label="edit" onClick={() => updateCharacterButton(character)}>
-                <EditIcon />
-              </IconButton>       
-              <IconButton aria-label="delete" onClick={() => deleteCharacter(character)}>
-                  <DeleteIcon />
-              </IconButton>
-            </Item> 
-          </Grid>
-        </Grid>
-      </Box>
-    </div>
-    </>);
+	const opacity = isDragging ? 0 : 1
+	drag(drop(ref))
+	return (
+		<>
+			<MonsterInfoDialog
+				open={open}
+				monsterInfo={monsterInfo!}
+				onClose={handleClose}
+			/>
+			<div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
+				<Box>
+					<Grid container spacing={2}>
+						<Grid item xs={1} sm={1}>
+							<Box className="item">{character.initiative}</Box>
+						</Grid>
+						<Grid item xs={5} sm={3}>
+							<Box className="item">
+								{character.name}
+								{
+									character.role == CharacterType.NonPlayer ?
+										(<IconButton aria-label="delete" onClick={() => getMonsterInfo(character.monster!)}>
+											<InfoIcon />
+										</IconButton>) : ''
+								}
+							</Box>
+						</Grid>
+						<Grid item xs={6} sm={3}>
+							<Box className="item">
+								<CharacterHp character={character} />
+							</Box>
+						</Grid>
+						<Grid item xs={8} sm={3}>
+							<Box className="item">
+								<CharacterConditions character={character} updateCharacter={updateCharacter} />
+							</Box>
+						</Grid>
+						<Grid item xs={4} sm={2}>
+							<Box className="item" style={{ "textAlign": "right" }}>
+								<IconButton aria-label="edit" onClick={() => updateCharacterButton(character)}>
+									<EditIcon />
+								</IconButton>
+								<IconButton aria-label="delete" onClick={() => deleteCharacter(character)}>
+									<DeleteIcon />
+								</IconButton>
+							</Box>
+						</Grid>
+					</Grid>
+				</Box>
+			</div>
+		</>);
 }
--- client/app/[sessionid]/dm/player-input-list.tsx
+++ client/app/[sessionid]/dm/player-input-list.tsx
@@ -1,17 +1,30 @@
 import { PlayerInput } from "@/app/_apis/playerInput"
-import { Button } from "@mui/material"
+import { Box, Button, Grid, styled } from "@mui/material"
 import { FC } from "react"
 
-export interface PlayerInputListProps{playerInputs: PlayerInput[], handleClickClearResults: () => void}
+export interface PlayerInputListProps { playerInputs: PlayerInput[], handleClickClearResults: () => void }
 
-export const PlayerInputList: FC<PlayerInputListProps> = ({playerInputs, handleClickClearResults}) => {
+export const PlayerInputList: FC<PlayerInputListProps> = ({ playerInputs, handleClickClearResults }) => {
+
     return (
         <div>
-        {playerInputs.map(input => (
-            <div key={input.client_uuid}>
-               Reason: {input.reason} Name: {input.name} - Value: {input.value}
-            </div>
-            ))} 
+            {playerInputs.map(input => (
+                <div key={input.client_uuid} style={{ border: '1px solid lightgray' }}>
+                    <Box>
+                        <Grid container spacing={2}>
+                            <Grid item xs={4} sm={4}>
+                                <Box className="item">{input.name}</Box>
+                            </Grid>
+                            <Grid item xs={4} sm={4}>
+                                <Box className="item">{input.reason}</Box>
+                            </Grid>
+                            <Grid item xs={4} sm={4}>
+                                <Box className="item">{input.value}</Box>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </div>
+            ))}
             <Button variant="contained" aria-label="end session" onClick={handleClickClearResults}>
                 Clear Results
             </Button>
