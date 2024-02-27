ebfbdfed4d0884339e1934afeff646db1266444c
joshuahoff1 <joshua.hoff1@gmail.com>
2024-02-27 13:38:14 -0600

    #17 allowed decimals for initiative order

--- src/scryer/creatures/creature.py
+++ src/scryer/creatures/creature.py
@@ -56,7 +56,7 @@ class CreatureV2(CreatureModel, Creature, metaclass=CreatureMeta):
     conditions:  list[str]
     creature_id: UUID
     hit_points:  HitPoints
-    initiative:  int
+    initiative:  float
     name:        str | None = None
     role:        Role = Role.NON_PLAYER
     monster:     str | None = None
