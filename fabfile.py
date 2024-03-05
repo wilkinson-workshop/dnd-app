e2a58d344c54147b59233af5ab5d90030c10512a
Scryer Admin <scryer@aabernathy-mc.lan>
2024-03-05 06:27:30 -0600

    direct declaration in fabfile

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,6 @@ class Config:
     hostname              = os.getenv("REMOTE_HOSTNAME")
     username              = os.getenv("REMOTE_USERNAME")
     application_path      = os.getenv("APPLICATION_PATH", "~")
-    service_name_frontend = os.getenv("SERVICE_NAME_FRONTEND", "")
-    service_name_backend  = os.getenv("SERVICE_NAME_BACKEND", "")
 
 
 def systemctl_run_userlevel(conn: Connection, name: str, command: str):
@@ -19,5 +17,5 @@ def deploy(_):
     with Connection(Config.hostname, Config.username) as c:
         with c.cd(Config.application_path):
             c.run("git pull")
-            systemctl_run_userlevel(c, Config.service_name_backend, "restart")
-            systemctl_run_userlevel(c, Config.service_name_frontend, "restart")
+            systemctl_run_userlevel(c, "scryer-test.service", "restart")
+            systemctl_run_userlevel(c, "scryer-client-test.service", "restart")
